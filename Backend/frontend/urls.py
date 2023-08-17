7208ce2708abf8295afbfcd3743f6c9c142dc97f
surender <surenderoffcl2002@gmail.com>
2023-08-17 16:46:09 +0530

    Added Login and Authentication in django for streamlit

--- Backend/frontend/urls.py
+++ Backend/frontend/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.landingpage,name="home"),
+    path('', views.landingpage, name='landingpage'),
+    # path('streamlit/',views.streamlit,name="stremlit_app"),
 ]
--- Backend/frontend/views.py
+++ Backend/frontend/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def landingpage(request):
-    return render(request, 'home.html')
+    print("Hey")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'streamlit.html') 
+        else:
+            error_message = "Invalid login credentials. Please try again."
+            return render(request, 'home.html', {'error_message': error_message})
+    
+    return render(request, 'home.html')  
+
+
